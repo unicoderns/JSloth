dc0da63c381cb5bd6c077be965005b1d28d70e45
Chriss Mejía <me@chrissmejia.com>
2018-09-06 20:50:50 -0600

    Admin login redirect on /admin/

--- source/system/apps/admin/controllers/index.ts
+++ source/system/apps/admin/controllers/index.ts
@@ -45,11 +45,12 @@ export default class IndexController extends HtmlController {
 
     /*** Define routes */
     protected routes(): void {
-        this.app.get("/", this.sessionsMiddleware.isAdmin("html"), this.index);
-        this.router.use("/", this.app);
+        this.app.get("/", this.sessionsMiddleware.isAdmin("redirect"),
+            this.sessionsMiddleware.isAdmin("html"), this.index);
         this.app.get("/users/", this.sessionsMiddleware.isAdmin("html"), this.users);
         this.app.get("/sessions/", this.sessionsMiddleware.isAdmin("html"), this.sessions);
         this.app.get("/contacts/", this.sessionsMiddleware.isAdmin("html"), this.contacts);
+        this.router.use("/", this.app);
     }
 
     /**
@@ -65,13 +66,13 @@ export default class IndexController extends HtmlController {
             page: "index"
         });
     };
-     /**
-     * Users view.
-     *
-     * @param req { Request } The request object.
-     * @param res { Response } The response object.
-     * @return html
-     */
+    /**
+    * Users view.
+    *
+    * @param req { Request } The request object.
+    * @param res { Response } The response object.
+    * @return html
+    */
     private users = (req: Request, res: Response): void => {
         this.render(req, res, "users", {
             title: "Users",
@@ -106,6 +107,6 @@ export default class IndexController extends HtmlController {
             page: "contacts"
         });
     };
-    
+
 
 }
